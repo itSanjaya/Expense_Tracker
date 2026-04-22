b6b5bbfbb09546de9b9a16d101c118430f459013
itSanjaya <113206972+itSanjaya@users.noreply.github.com>
2026-04-22 21:22:16 +0545

    Add budget routes to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ import expenseRoutes from "./routes/expenseRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import authMiddleware from "./middleware/authMiddleware.js";
+import budgetRoutes from "./routes/budgetRoutes.js";
 
 const app = express();
 
@@ -13,12 +14,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: process.env.CLIENT_URL || "http://localhost:5000",
   credentials: true,
 }));
 
 app.use("/auth", authRoutes);
 app.use("/expenses", authMiddleware, expenseRoutes);
 app.use("/categories", authMiddleware, categoryRoutes);
+app.use("/budgets", authMiddleware, budgetRoutes);
 
 export default app;
