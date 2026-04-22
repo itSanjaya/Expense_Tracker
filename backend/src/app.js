60d5dbd25615ba9b15e476996e45ba4061409719
itSanjaya <113206972+itSanjaya@users.noreply.github.com>
2026-04-22 21:30:40 +0545

    Refactor CORS settings in app.js
    Updated CORS configuration to use CLIENT_URL directly.

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5000",
+  //production
+  origin: process.env.CLIENT_URL ,
   credentials: true,
 }));
 
