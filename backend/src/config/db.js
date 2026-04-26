42b507b310582ee4e50d4c9a834ba1d2d984157f
itSanjaya <113206972+itSanjaya@users.noreply.github.com>
2026-04-26 23:58:50 +0545

    Refactor database connection setup in db.js
    Updated database connection configuration to use DATABASE_URL.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,23 +1,23 @@
-// import pkg from "pg";
-// const { Pool } = pkg;
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
-// });
-
-// export default pool;
-
-
 import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
+
+
+// import pkg from "pg";
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
+// export default pool;
